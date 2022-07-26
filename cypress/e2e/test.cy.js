c34b32cd40788a318e69b5b1087fc33208e04672
Eli Davidson <eli@shadowlab.dev>
2022-07-26 16:43:28 -0600

    Update tests

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,8 +1,3 @@
-const validName = 'Name Lastnamington III'
-const validDate = '2022-01-21' 
-const validTime = '23:00:00' 
-const validNumber = '24' 
-
 beforeEach(() => { 
   //cy.intercept('GET', '/reservations', {fixture: 'reservations.json'}).as('getReservations') 
   //cy.intercept('POST', '/reservations', {fixture: 'reservations.json'}).as('postReservation') 
@@ -17,11 +12,11 @@ describe('User Stories', () => {
 })
 
 describe('App', () => {
-    it.skip('Should exist', () => {
+    it('Should exist', () => {
         cy.get('.App').should('exist')
     })
 
-    it.skip('Should contain a name', () => {
+    it('Should contain a name', () => {
         cy.get('.App').within(() => {
             cy.get('p').should('exist')
             cy.get('p').invoke('text').should('eq', 'App')
