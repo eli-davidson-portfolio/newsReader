eef0ce997d95f3619bd688492913839c9a5f5758
Eli Davidson <eli@shadowlab.dev>
2022-07-26 16:29:56 -0600

    Setup and test cypress

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,17 @@
+const validName = 'Name Lastnamington III'
+const validDate = '2022-01-21' 
+const validTime = '23:00:00' 
+const validNumber = '24' 
+
+beforeEach(() => { 
+  //cy.intercept('GET', '/reservations', {fixture: 'reservations.json'}).as('getReservations') 
+  //cy.intercept('POST', '/reservations', {fixture: 'reservations.json'}).as('postReservation') 
+  //cy.intercept('DELETE', '/reservations', {fixture: 'reservations.json'}).as('deleteReservation') 
+  //cy.visit('localhost:3000') 
+}) 
+
+describe('User Stories', () => {
+  it('Confirm tests are working', () => {
+      expect(true).to.equal(true)
+  })
+})
