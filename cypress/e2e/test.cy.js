2e63714bcd047a536e9591f61d234d9df1fe3c2f
Eli Davidson <eli@shadowlab.dev>
2022-07-26 18:44:55 -0600

    Update test file syntax

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -13,23 +13,15 @@ describe('App', () => {
     it('Should exist', () => {
         cy.get('.App').should('exist')
     })
-
-    it('Should contain a name', () => {
-        cy.get('.App').within(() => {
-            cy.get('p').should('exist')
-            cy.get('p').invoke('text').should('eq', 'App')
-       })
-   })
-
-   
+ 
 })
 
 describe('ArticleContainer', () => {
-    it.skip('Should exist', () => {
+    it('Should exist', () => {
         cy.get('.ArticleContainer').should('exist')
     })
 
-    it.skip('Should contain a name', () => {
+    it('Should contain a name', () => {
         cy.get('.ArticleContainer').within(() => {
             cy.get('p').should('exist')
             cy.get('p').invoke('text').should('eq', 'ArticleContainer')
@@ -37,33 +29,12 @@ describe('ArticleContainer', () => {
    })
 })
 
-
-
-
-const validName = 'Name Lastnamington III'
-const validDate = '2022-01-21' 
-const validTime = '23:00:00' 
-const validNumber = '24' 
-
-beforeEach(() => { 
-  //cy.intercept('GET', '/reservations', {fixture: 'reservations.json'}).as('getReservations') 
-  //cy.intercept('POST', '/reservations', {fixture: 'reservations.json'}).as('postReservation') 
-  //cy.intercept('DELETE', '/reservations', {fixture: 'reservations.json'}).as('deleteReservation') 
-  //cy.visit('localhost:3000') 
-}) 
-
-describe('User Stories', () => {
-  it('Confirm tests are working', () => {
-      expect(true).to.equal(true)
-  })
-})
-
 describe('Article', () => {
-    it.skip('Should exist', () => {
+    it('Should exist', () => {
         cy.get('.Article').should('exist')
     })
 
-    it.skip('Should contain a name', () => {
+    it('Should contain a name', () => {
         cy.get('.Article').within(() => {
             cy.get('p').should('exist')
             cy.get('p').invoke('text').should('eq', 'Article')
