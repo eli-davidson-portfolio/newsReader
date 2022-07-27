533f1b7744068b4e49fdf7628373c24f1f8403e6
Eli Davidson <eli@shadowlab.dev>
2022-07-26 18:37:37 -0600

    Update test file syntax

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -9,7 +9,7 @@ describe('User Stories', () => {
   })
 })
 
-describe('App', () => {
+  describe('App', () => {
     it('Should exist', () => {
         cy.get('.App').should('exist')
     })
@@ -21,37 +21,17 @@ describe('App', () => {
        })
    })
 
-   
-})
+  describe('ArticleContainer', () => {
+      it.skip('Should exist', () => {
+          cy.get('.ArticleContainer').should('exist')
+      })
 
-const validName = 'Name Lastnamington III'
-const validDate = '2022-01-21' 
-const validTime = '23:00:00' 
-const validNumber = '24' 
-
-beforeEach(() => { 
-  //cy.intercept('GET', '/reservations', {fixture: 'reservations.json'}).as('getReservations') 
-  //cy.intercept('POST', '/reservations', {fixture: 'reservations.json'}).as('postReservation') 
-  //cy.intercept('DELETE', '/reservations', {fixture: 'reservations.json'}).as('deleteReservation') 
-  //cy.visit('localhost:3000') 
-}) 
-
-describe('User Stories', () => {
-  it('Confirm tests are working', () => {
-      expect(true).to.equal(true)
-  })
-})
-
-describe('ArticleContainer', () => {
-    it.skip('Should exist', () => {
-        cy.get('.ArticleContainer').should('exist')
+      it.skip('Should contain a name', () => {
+          cy.get('.ArticleContainer').within(() => {
+              cy.get('p').should('exist')
+              cy.get('p').invoke('text').should('eq', 'ArticleContainer')
+        })
     })
+  })
 
-    it.skip('Should contain a name', () => {
-        cy.get('.ArticleContainer').within(() => {
-            cy.get('p').should('exist')
-            cy.get('p').invoke('text').should('eq', 'ArticleContainer')
-       })
-   })
 })
-
