65d98dfb1cead515323b782fad64744e390b3619
Eli Davidson <eli@shadowlab.dev>
2022-07-26 18:34:57 -0600

    Create ArticleContainer

---
+++ src/components/ArticleContainer.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import '../styles/ArticleContainer.scss'
+ 
+export function ArticleContainer() { 
+  const [id, setID] = useState('') 
+ 
+  useEffect(() => { 
+    console.log('ArticleContainer did mount') 
+ 
+    return(() => { 
+    console.log('ArticleContainer will unmount') 
+    }) 
+  }, []) 
+ 
+   useEffect(() => { 
+    console.log('ArticleContainer did update') 
+ 
+ 
+  }, [id]) 
+ 
+  return ( 
+    <div className='ArticleContainer'> 
+      <p>ArticleContainer</p> 
+    </div> 
+  ); 
+} 
+ 
+//import { ArticleContainer } from '../components/ArticleContainer' 
+//<ArticleContainer />' 
