f35a3c305d122d6a7bbae9813471b0708872961a
Eli Davidson <eli@shadowlab.dev>
2022-07-26 18:07:30 -0600

    Add API endpoint, connect to app component

--- src/classes/apiEndpoints.js
+++ src/classes/apiEndpoints.js
@@ -1,6 +1,7 @@
-const url = 'URL'
- 
-async function getData() {
+const apiKey = 'cOiIGzc9b1A5xajV0IfRb2CfbC2DVrVI'
+
+export async function getData(section) {
+    const url = `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${apiKey}`
     const response = await fetch(url)
 
     switch(response.status) {
@@ -12,41 +13,5 @@ async function getData() {
     }
 }
 
-async function postData(data) {
-    const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data)
-    });
  
-    switch(response.status) {
-      case 201:
-        return response.json()
-        break;
-      default:
-        throw Error(response.statusText);
-    }
-}
- 
-async function deleteData(id) {
-    const response = await fetch(url + '/' + id, {
-    method: 'DELETE',
-    });
-    
-    switch(response.status) {
-    case 202:
-        return response.json()
-        break;
-default:
-        throw Error(response.statusText);
-    }
-}
- 
-export {getData, postData, deleteData}
- 
-//import { deleteData, getData, postData } from '../classes/apiEndpoints'
-//getData().then(data => console.log(data)).catch(error => alert(error));
-//postData(data).then(data => console.log(data)).catch(error => alert(error));
-//deleteData(id).then(data => console.log(data)).catch(error => alert(error));
+
