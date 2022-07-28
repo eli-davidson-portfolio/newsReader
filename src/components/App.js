bf136f3487076c300a4bd4dc6ef669fc04cbbfd6
Eli Davidson <eli@shadowlab.dev>
2022-07-28 09:46:24 -0600

    Update components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import '../styles/App.scss'
 import { ArticleContainer } from '../components/ArticleContainer' 
 import { ArticleDetail } from '../components/ArticleDetail' 
